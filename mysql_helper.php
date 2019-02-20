88be9dc405df8b59a4586a9661f7cb7f8c6acd7d
Алексей Иванов <Alessio18911@gmail.com>
2019-02-20 12:59:28 +0300

    Правки

--- mysql_helper.php
+++ mysql_helper.php
@@ -46,12 +46,6 @@ function getTasksForCategory($link, int $user_id, int $category_id = null): arra
             JOIN users ON categories.user_id = users.id
             WHERE users.id = $user_id";
     } else {
-        $category_id_max = mysqli_fetch_assoc(mysqli_query($link, "SELECT MAX(id) AS category_id_max FROM categories"))['category_id_max'];
-
-        if($category_id > $category_id_max) {
-            die(http_response_code(404));
-        }
-
         $sql_tasks = "SELECT tasks.name, tasks.created_at, tasks.expires_at, categories.name AS categories_name, status FROM tasks
             JOIN categories ON tasks.category_id = categories.id
             JOIN users ON categories.user_id = users.id
