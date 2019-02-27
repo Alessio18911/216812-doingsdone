7a7785d849042ceca8ee3780f3f2f4462d9c3726
Алексей Иванов <Alessio18911@gmail.com>
2019-02-27 20:19:35 +0300

    Подготовка (продолжение)

--- identification.php
+++ identification.php
@@ -1,1 +1,18 @@
 <?php
+
+require_once('functions.php');
+require_once('mysql_helper.php');
+
+$content = '';
+
+if(isset($_GET['register'])) {
+    $content = include_template('templates/register', []);
+} else if(isset($_GET['authorize'])) {
+    $content = include_template('template/authorize', []);
+}
+
+$layout_content = include_template('identification_layout.php', [
+    'content' => $content
+])
+
+print($layout_сontent);
