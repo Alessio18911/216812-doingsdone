18f0dadb3f0abbb1ae5ed1b6424be2290a4a2d1f
Алексей Иванов <Alessio18911@gmail.com>
2019-02-27 20:39:15 +0300

    Подготовка (продолжение)

--- identify.php
+++ identify.php
@@ -5,12 +5,12 @@ require_once('mysql_helper.php');
 
 $connection = getConnection('216812-doingsdone', 'root', '', 'doingsdone');
 
-$content = '';
+$content = include_template('register.php', []);;
 
 if(isset($_GET['register'])) {
-    $content = include_template('templates/register', []);
+    $content = include_template('register.php', []);
 } else if(isset($_GET['authorize'])) {
-    $content = include_template('template/authorize', []);
+    $content = include_template('authorize.php', []);
 }
 
 $layout_content = include_template('identify_layout.php', [
